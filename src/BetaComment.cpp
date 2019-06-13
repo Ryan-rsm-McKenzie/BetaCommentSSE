438885d688f5689117e28fa9456c27e3da08a962
ryan-rsm-mckenzie <ryan__mckenzie@hotmail.com>
2019-06-12 18:34:00 -0700

    1.1.0
    * Added formID and adjusted format

--- src/BetaComment.cpp
+++ src/BetaComment.cpp
@@ -95,6 +95,7 @@ void BetaComment::CPrint(const char* a_fmt, ...)
 
 void BetaComment::LogComment(RE::TESObjectREFR* a_ref, const std::string& a_comment)
 {
+	constexpr char DELIM = '\t';
 	std::stringstream line;
 
 	auto time = std::time(0);
@@ -104,7 +105,7 @@ void BetaComment::LogComment(RE::TESObjectREFR* a_ref, const std::string& a_comm
 		CPrint("> [BetaComment] ERROR: Failed to get local time with error code (%i)", err);
 		return;
 	} else {
-		line << std::put_time(&localTime, "%m/%d/%Y (%H:%M)") << ' ';
+		line << std::put_time(&localTime, "%m/%d/%Y (%H:%M)") << DELIM;
 	}
 
 	auto source = a_ref->GetFinalSourceFile();
@@ -112,7 +113,7 @@ void BetaComment::LogComment(RE::TESObjectREFR* a_ref, const std::string& a_comm
 		CPrint("> [BetaComment] ERROR: Failed to get last modified source file for selected ref", err);
 		return;
 	} else {
-		line << source->name << ' ';
+		line << source->name << DELIM;
 	}
 
 	SYSTEMTIME fileTime;
@@ -121,18 +122,24 @@ void BetaComment::LogComment(RE::TESObjectREFR* a_ref, const std::string& a_comm
 		CPrint("> [BetaComment] ERROR: Failed to convert file time to system time with error code (%i)", GetLastError());
 		return;
 	} else {
-		line << fileTime.wMonth << '/' << fileTime.wDay << '/' << fileTime.wYear << " (" << fileTime.wHour << ':' << fileTime.wSecond << ") ";
+		line << fileTime.wMonth << '/' << fileTime.wDay << '/' << fileTime.wYear << " (" << fileTime.wHour << ':' << fileTime.wSecond << ')' << DELIM;
 	}
 
-	line << _userName << ' ';
+	line << _userName << DELIM;
 
 	auto cell = a_ref->GetParentCell();
+	line << cell->GetEditorID() << DELIM;
+
+	char formID[] = "0xDEADBEEF";
+	std::snprintf(formID, sizeof(formID), "0x%08X", cell->formID);
+	line << formID << DELIM;
+
 	auto coordinates = cell->GetCoordinates();
 	if (coordinates) {
-		line << '(' << coordinates->x << ',' << coordinates->y << ") ";
+		line << '(' << coordinates->x << ',' << coordinates->y << ')' << DELIM;
 	}
 
-	line << std::floor(a_ref->pos.x) << ' ' << std::floor(a_ref->pos.y) << ' ' << std::floor(a_ref->pos.z) << ' ';
+	line << std::floor(a_ref->pos.x) << ' ' << std::floor(a_ref->pos.y) << ' ' << std::floor(a_ref->pos.z) << DELIM;
 	line << '\"' << a_comment << '\"';
 
 	_file << line.str() << '\n';
